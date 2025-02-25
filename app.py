3e379b54d93e048c579863fa721d1c1ba830cf22
Andrew Crossan <andrew.crossan@gmail.com>
2025-02-25 23:59:47 +0000

    flask app

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-def hello():
-    print("Helloooo")
+from flask import Flask
 
-hello()
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
